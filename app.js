c8a34ae7382ad9d8c3674b42674bc9e140dace59
Bitrey <info@bitrey.it>
2020-02-08 18:58:08 +0100

    cancella messaggi

--- app.js
+++ app.js
@@ -7,24 +7,27 @@ require('dotenv').config();
 app.set("view engine", "ejs");
 
 // CONNECT MONGODB URI
-mongoose.connect(process.env.mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(){
+mongoose.connect(process.env.mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, function(){
     console.log("Database connesso!");
 });
 
 // SET MESSAGE SCHEMA
 var MessageSchema = new mongoose.Schema({
     username: String,
-    message: String
+    message: String,
+    date: { type: Date, default: Date.now },
+    socket_id: String
 });
-var Message = mongoose.model("Message", MessageSchema)
-
-var sendMessage;
+var Message = mongoose.model("Message", MessageSchema);
 
 app.get("/", function(req, res){
     Message.find({}, function(err, messageFound){
-        sendMessage = messageFound;
+        if(err){
+            res.status(204).send("Si è verificato un errore nel caricamento");
+        } else {
+            res.render("index", messageFound);
+        }
     });
-    res.render("index", sendMessage);
 });
 
 app.get("/informazioni", function(req, res){
@@ -50,23 +53,35 @@ var io = socket(server);
 
 io.on("connection", function(socket){
     console.log("Nuova connessione da " + socket.id);
-    socket.on("chat", function(data){
-        io.sockets.emit("chat", data);
-        Message.create({
-            username: data.username,
-            message: data.message
-        }, function(err, saved){
-            if(err){
-                console.log(err);
-            };
-        });
-    });
-    
     Message.find({}, function(err, messages){
-        socket.emit("pastMsg", messages);
+        if(err){
+            socket.emit("err", err);
+        } else {
+            socket.emit("pastMsg", messages);
+        }
+    });
+    socket.on("chat", function(data){
+        let messageObj = new Message({
+            message: data.message,
+            username: data.username,
+            date: Date.now(),
+            socket_id: socket.id
+        })
+        messageObj.save(function(err){if(err){console.log(err);}});
+        io.sockets.emit("chat", messageObj);
     });
 
     socket.on("typing", function(data){
         socket.broadcast.emit("typing", data);
+    })
+
+    socket.on("cancella", function(data){
+        Message.findOneAndRemove({date: data}, function(err, deleted){
+            if(err){
+                console.log(err);
+            } else {
+                socket.emit("cancella", deleted);
+            }
+        });
     })
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,9 @@
 var socket = io.connect();
 $(".alert").hide();
 // Query DOM
+
+var chat_window = document.getElementById("chat-window");
+
 var message = document.getElementById("message"),
     username = document.getElementById("username"),
     btn = document.getElementById("send"),
@@ -36,21 +39,45 @@ message.addEventListener("keyup", function(){
     };
 });
 
+var unread = 0;
+
 // Listen for events
 socket.on("chat", function(data) {
     feedback.innerHTML = "";
-    output.innerHTML += "<p><strong>" + data.username + "</strong> " + data.message + "</p>";
+    if(data.socket_id == socket.id){
+        output.innerHTML += "<p id='" + data.date + "'><span class='delete'><i class='fa fa-trash' aria-hidden='true'></i></span> <strong>" + data.username + "</strong> " + data.message + "</p>";
+    } else {
+        output.innerHTML += "<p id='" + data.date + "'><strong>" + data.username + "</strong> " + data.message + "</p>";
+    };
+    chat_window.scrollTop = chat_window.scrollHeight;
+    unread++;
+    document.title = "(" + unread + ") 3F Chat";
 });
 
 socket.on("typing", function(data) {
     feedback.innerHTML = "<p><em>" + data + " sta scrivendo...</em></p>"
+    chat_window.scrollTop = chat_window.scrollHeight;
 });
 
-socket.on("pastMsg", function(data){
-    data.forEach(function(message){
-        output.innerHTML += "<p><strong>" + message.username + "</strong> " + message.message + "</p>";
-    });
-    console.log(socket.id);
+setInterval(function(){
+    if(document.hidden){
+        if(unread != 0){
+            document.title = "(" + unread + ") 3F Chat";
+        }
+    } else {
+        unread = 0;
+        document.title = "3F Chat";
+    }
+}, 500);
+
+socket.on("pastMsg", function(messages){
+    $("#output").html("");
+    for(let i = 0; i < messages.length; i++){
+        unread++;
+        document.title = "(" + unread + ") 3F Chat";
+        output.innerHTML += "<p id='" + messages[i].date + "'><strong>" + messages[i].username + "</strong> " + messages[i].message + "</p>";
+    }
+    chat_window.scrollTop = chat_window.scrollHeight;
 });
 
 $(".hideMe").hide();
@@ -62,10 +89,23 @@ function magia(){
     if(sfondo == false){
         $("#chat-window").css("background", "#ffffff88");
         $("#chat-window").css("background-image", "url('https://s5.gifyu.com/images/ezgif-2-52ade8a765d5.gif')");
+        $("#footer-img-left").attr("src", "https://www.googleapis.com/drive/v3/files/1K8QMR8ETZbb0kP0pGRl8noQB-XaR78nn?alt=media&key=AIzaSyCzJtUQTqW3tZTuLnq4b8EvfZlZqhaw5Hw");
+        $("#footer-img-right").attr("src", "https://www.googleapis.com/drive/v3/files/1K8QMR8ETZbb0kP0pGRl8noQB-XaR78nn?alt=media&key=AIzaSyCzJtUQTqW3tZTuLnq4b8EvfZlZqhaw5Hw");
         sfondo = true;
     } else {
         $("#chat-window").css("background", "#f9f9f9");
         $("#chat-window").css("background-image", "");
+        $("#footer-img-left").attr("src", "https://i.imgur.com/P60WYPZ.png");
+        $("#footer-img-right").attr("src", "https://i.imgur.com/lDIspTu.png");
         sfondo = false;
     };
 };
+
+$("#output").on("click", ".delete", function(){
+    socket.emit("cancella", $(this).parent().attr("id"));
+	$(this).parent().html('<p id="loading" class="text-center pt-3"><span class="spinner-border spinner-border-sm text-danger" role="status"><span class="sr-only">Rimozione...</span></span> Rimozione del messaggio</p>');
+});
+
+socket.on("cancella", function(data){
+    setTimeout(function(){$(`#${data.date}`).remove();}, 100);
+});
